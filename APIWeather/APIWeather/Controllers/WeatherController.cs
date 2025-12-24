82bbd75d9def4d8827149adfa29b9f6a50c58286
nazkamberoglu <naz.kamberoglu199@gmail.com>
2025-12-24 22:09:05 +0300

    Weather API Application
    This project is a web application developed using a Weather API.
    Current weather information for the selected city is fetched in real time from the API and displayed to the user.
    
    The application provides live data such as temperature, feels-like temperature, humidity, wind speed, and weather conditions.
    
    Features
    
    Weather API Integration
    
    Fetch current weather data by city
    
    Real-time data usage
    
    City-Based Weather Information
    
    Display weather details for the selected city
    
    Temperature, humidity, wind speed, and weather description
    
    Live Data Display
    
    Current and feels-like temperature
    
    Humidity and wind speed information
    
    User Experience
    
    Simple and user-friendly interface
    
    Dynamic data updates
    
    Technologies Used
    
    ASP.NET Core MVC
    
    C#
    
    RESTful API
    
    Weather API
    
    JSON
    
    HTML / CSS / Bootstrap
    
    JavaScript

---
+++ APIWeather/APIWeather/Controllers/WeatherController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using static APIWeather.Models.Class;
+using static System.Net.WebRequestMethods;
+
+namespace APIWeather.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private List<string> sehirler = new List<string>
+   {
+    "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya",
+    "Ardahan", "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", "Bitlis",
+    "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce",
+    "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane",
+    "Hakkari", "Hatay", "Iğdır", "Isparta", "İstanbul", "İzmir", "Kahramanmaraş", "Karabük", "Karaman",
+    "Kars", "Kastamonu", "Kayseri", "Kilis", "Kırıkkale", "Kırklareli", "Kırşehir", "Kocaeli", "Konya",
+    "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş", "Nevşehir", "Niğde",
+    "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Şanlıurfa", "Şırnak",
+    "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak"
+   };
+
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new VM
+            {
+                Cities = sehirler
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(VM model)
+        {
+            if (!string.IsNullOrEmpty(model.City))
+            {
+                string url = $"http://api.weatherapi.com/v1/current.json?key=6af3d3efda4e4ada9eb160826252103&q={model.City}&aqi=no=";
+                string json = new WebClient().DownloadString(url);
+                model.root = JsonConvert.DeserializeObject<Root>(json);
+            }
+
+           
+            model.Cities = sehirler;
+
+            return View(model);
+        }
+    }
+}
---
+++ APIWeather/APIWeather/Models/Class.cs
@@ -0,0 +1,76 @@
+﻿
+namespace APIWeather.Models
+{
+    public class Class
+    {
+        public class Condition
+        {
+            public string text { get; set; }
+            public string icon { get; set; }
+            public int code { get; set; }
+        }
+
+        public class Current
+        {
+            public int last_updated_epoch { get; set; }
+            public string last_updated { get; set; }
+            public double temp_c { get; set; }
+            public double temp_f { get; set; }
+            public int is_day { get; set; }
+            public Condition condition { get; set; }
+            public double wind_mph { get; set; }
+            public double wind_kph { get; set; }
+            public double wind_degree { get; set; }
+            public string wind_dir { get; set; }
+            public double pressure_mb { get; set; }
+            public double pressure_in { get; set; }
+            public double precip_mm { get; set; }
+            public double precip_in { get; set; }
+            public double humidity { get; set; }
+            public double cloud { get; set; }
+            public double feelslike_c { get; set; }
+            public double feelslike_f { get; set; }
+            public double windchill_c { get; set; }
+            public double windchill_f { get; set; }
+            public double heatindex_c { get; set; }
+            public double heatindex_f { get; set; }
+            public double dewpoint_c { get; set; }
+            public double dewpoint_f { get; set; }
+            public double vis_km { get; set; }
+            public double vis_miles { get; set; }
+            public double uv { get; set; }
+            public double gust_mph { get; set; }
+            public double gust_kph { get; set; }
+        }
+
+        public class Location
+        {
+            public string name { get; set; }
+            public string region { get; set; }
+            public string country { get; set; }
+            public double lat { get; set; }
+            public double lon { get; set; }
+            public string tz_id { get; set; }
+            public int localtime_epoch { get; set; }
+            public string localtime { get; set; }
+        }
+
+        public class Root
+        {
+            public Location location { get; set; }
+            public Current current { get; set; }
+        }
+
+        public class VM
+        {
+           public Root root { get; set; }
+
+            public List<string> Cities { get; set; }
+
+            public string City {  get; set; }
+
+           
+        }
+
+    }
+}
